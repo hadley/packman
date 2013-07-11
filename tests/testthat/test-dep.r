9e1cbe92bb84c1827eb2912ca376e9929096f5eb
hadley <h.wickham@gmail.com>
2013-07-11 09:52:50 +0200

    Add test on cran dep

--- tests/testthat/test-dep.r
+++ tests/testthat/test-dep.r
@@ -9,4 +9,16 @@ test_that("single local dep resolved", {
   deps_a <- find_dependencies("a", sources = local_dep)
   expect_equal(length(deps_a), 1)
   expect_equal(deps_a[[1]]$Package, "b")
+  expect_false(is.installed(deps_a[[1]]))
+})
+
+test_that("single local dep resolved", {
+  local_dep <- test_source("local-dep")
+  
+  deps_b <- find_dependencies("b", sources = local_dep)
+  expect_equal(length(deps_b), 0)
+  
+  deps_a <- find_dependencies("a", sources = local_dep)
+  expect_equal(length(deps_a), 1)
+  expect_equal(deps_a[[1]]$Package, "b")
 })
